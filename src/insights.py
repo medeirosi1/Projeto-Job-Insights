63c92fbc4825f62e9f7bd1dcf672fe5262160090
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-20 18:36:18 -0300

    feat: filter by job type

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,9 @@ def get_unique_job_types(path):
 # get_unique_job_types('src/jobs.csv')
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    jobType = [job for job in  jobs if job['job_type'] == job_type]
+    print(jobType)
+    return jobType
 
 
 def get_unique_industries(path):

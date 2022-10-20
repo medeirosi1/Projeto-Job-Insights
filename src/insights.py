3d953e60dfd7d22a21a1eede295dc7b27368729b
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-20 18:36:53 -0300

    feat: filter by industry

--- src/insights.py
+++ src/insights.py
@@ -16,22 +16,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    readJobs = read(path)
+    jobTypes = [read['industry'] for read in readJobs if read['industry'] != '']
+    print(list(set(jobTypes)))
+    return list(set(jobTypes))
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
-
+# get_unique_industries('src/jobs.csv')
 
 def filter_by_industry(jobs, industry):
     """Filters a list of jobs by industry

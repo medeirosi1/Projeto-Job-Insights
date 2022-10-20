62dd66fb74ea88f2cb46e7d5ec2d4bf69ae437f1
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-20 15:30:52 -0300

    feat: read csv with dictReader

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,11 @@
 from functools import lru_cache
-
+import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path, encoding= "utf-8") as file:
+        reader = csv.DictReader(file, delimiter= "," )
+        return list(reader)
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+read('src/jobs.csv')

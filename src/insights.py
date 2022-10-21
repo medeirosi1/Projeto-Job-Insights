ff98765e3cfa63a1cc4560dd230ecab98a0577b3
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-21 10:07:03 -0300

    refactor: insights functions

--- src/insights.py
+++ src/insights.py
@@ -1,62 +1,56 @@
 from src.jobs import read
-import csv
+
 
 def get_unique_job_types(path):
     readJobs = read(path)
-    jobTypes = [read['job_type'] for read in readJobs ]
-    print(list(set(jobTypes)))
+    jobTypes = [read['job_type'] for read in readJobs]
     return list(set(jobTypes))
 
-# get_unique_job_types('src/jobs.csv')
 
 def filter_by_job_type(jobs, job_type):
-    jobType = [job for job in  jobs if job['job_type'] == job_type]
+    jobType = [job for job in jobs if job['job_type'] == job_type]
     print(jobType)
     return jobType
 
 
 def get_unique_industries(path):
-    readJobs = read(path)
-    jobTypes = [read['industry'] for read in readJobs if read['industry'] != '']
+    readTop = read(path)
+    jobTypes = [read['industry'] for read in readTop if read['industry'] != '']
     print(list(set(jobTypes)))
     return list(set(jobTypes))
 
-# get_unique_industries('src/jobs.csv')
 
 def filter_by_industry(jobs, industry):
-    industry_filter = [job for job in  jobs if job['industry'] == industry]
+    industry_filter = [job for job in jobs if job['industry'] == industry]
     print(industry_filter)
     return industry_filter
 
 
 def get_max_salary(path):
     readJobs = read(path)
-    maxSalary = [int(read['max_salary']) for read in readJobs if read['max_salary'].isdigit()]
-    print(maxSalary)
-    print(max(maxSalary))
+    maxSalary = [int(read['max_salary']) for read in readJobs if (
+        read['max_salary'].isdigit()
+    )]
     return max(maxSalary)
 
-# get_max_salary('src/jobs.csv')
 
 def get_min_salary(path):
     readJobs = read(path)
-    minSalary = [int(read['min_salary']) for read in readJobs if read['min_salary'].isdigit()]
-    print(minSalary)
-    print(min(minSalary))
+    minSalary = [int(read['min_salary']) for read in readJobs if (
+        read['min_salary'].isdigit()
+        )]
     return min(minSalary)
 
 
 def matches_salary_range(job, salary):
-    if job.get('min_salary') is None or job.get('max_salary') is None:
-        raise ValueError
-
-    if type(job['max_salary']) != int or type(job['min_salary']) != int:
-        raise ValueError
-
-    if job['min_salary'] > job['max_salary']:
-        raise ValueError
-
-    if type(salary) != int:
+    if(
+        job.get('min_salary') is None
+        or job.get('max_salary') is None
+        or (type(job['min_salary']) is not int)
+        or (type(job['max_salary']) is not int)
+        or job['min_salary'] > job['max_salary']
+        or (type(salary) is not int)
+    ):
         raise ValueError
 
     if job['min_salary'] <= salary <= job['max_salary']:
@@ -65,21 +59,7 @@ def matches_salary_range(job, salary):
         return False
         
 
-
-
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    salary_filter = [job for job in jobs if matches_salary_range(job, salary)]
+    print(salary_filter)
+    return salary_filter

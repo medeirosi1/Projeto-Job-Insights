58e3d84ca93a9c8229a572f5232fe04b75ccc485
Thiago Medeiros <medeiroshs@hotmail.com>
2022-10-21 09:41:22 -0300

    refactor: jobs function read

--- src/jobs.py
+++ src/jobs.py
@@ -1,11 +1,9 @@
-from functools import lru_cache
 import csv
+from functools import lru_cache
+
 
 @lru_cache
 def read(path):
-    with open(path, encoding= "utf-8") as file:
-        reader = csv.DictReader(file, delimiter= "," )
+    with open(path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",")
         return list(reader)
-
-
-read('src/jobs.csv')
